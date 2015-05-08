1f8450b27a35f8cd17c204e20b63d081b9bcf4a0
Chris Albright <calbright@cj.com>
2015-05-08 11:12:04 -0700

    last commit somehow lost producer code :hankey: (#1)

--- src/main/scala/com/cj/kafka/rx/package.scala
+++ src/main/scala/com/cj/kafka/rx/package.scala
@@ -1,13 +1,16 @@
 package com.cj.kafka
 
-import java.util.Properties
-
-import _root_.rx.lang.scala.Observable
-import kafka.consumer.ConsumerConfig
-import kafka.message.MessageAndMetadata
-import org.apache.curator.utils.ZKPaths
+import org.apache.kafka.clients.producer.RecordMetadata
 
 package object rx {
+
+  import _root_.rx.lang.scala.Observable
+  import kafka.message.MessageAndMetadata
+  import org.apache.curator.utils.ZKPaths
+  import org.apache.kafka.clients.producer.{Producer, ProducerRecord}
+
+  import scala.language.implicitConversions
+
   type TopicPartition = (String, Int)
   type OffsetMap = Map[TopicPartition, Long]
   type KafkaIterable = Iterable[MessageAndMetadata[Array[Byte], Array[Byte]]]
@@ -17,26 +20,45 @@ package object rx {
   type PartialCommit = (OffsetMap, OffsetMerge) => OffsetMap
   type Commit = (OffsetMap, OffsetMerge, OffsetMerge) => OffsetMap
 
-  // simple analog to kafka's ConsumerConfig
-  case class SimpleConfig(zookeepers: String, group: String, autocommit: Boolean = false, startFromLatest: Boolean = false)
 
   def getPartitionPath(group: String, topic: String, part: Int) =
     ZKPaths.makePath(s"/consumers/$group/offsets/$topic", part.toString)
 
-  def getConsumerConfig(config: SimpleConfig) = {
-    val props = new Properties()
-    props.put("group.id", config.group)
-    props.put("zookeeper.connect", config.zookeepers)
-    props.put("auto.offset.reset", if (config.startFromLatest) "largest" else "smallest")
-    props.put("auto.commit.enable", if (config.autocommit) "true" else "false")
-    new ConsumerConfig(props)
-  }
-
   def copyMessage(message: MessageAndMetadata[Array[Byte], Array[Byte]]): Message[Array[Byte]] = {
-    Message(value=message.message(), topic=message.topic, partition=message.partition, offset=message.offset)
+    Message(value = message.message(), topic = message.topic, partition = message.partition, offset = message.offset)
   }
 
   def copyMessage(message: MessageAndMetadata[Array[Byte], Array[Byte]], offsets: OffsetMap, checkpoint: PartialCommit) = {
     Message(value = message.message(), topic = message.topic, partition = message.partition, offset = message.offset, offsets = offsets, commitWith = checkpoint)
   }
+
+  implicit class MessageTransformer[T](stream: Observable[Message[T]]) {
+    def transform[K, V](fn: Message[T] => ProducerMessage[K, V]): Observable[ProducerMessage[K, V]] = stream map fn
+  }
+
+  implicit def toProducerRecord[K, V](message: ProducerMessage[K, V]): ProducerRecord[K, V] = {
+    for {
+      key <- message.key.orNull
+      value <- message.value
+      partition <- message.partition.orNull
+      topic <- message.topic
+      if message.valid
+    } yield new ProducerRecord[K, V](topic, partition, key, value)
+  }
+
+  implicit class ProducerObservable[K, V](stream: Observable[ProducerMessage[K, V]]) {
+    def saveToKafka(topic: String)(implicit producer: Producer[K, V]): Observable[(RecordMetadata, OffsetMerge => OffsetMap)] = {
+      stream.map { message =>
+        val m: ProducerMessage[K, V] = message.copy(topic = Some(topic))
+        m
+      } map { message =>
+        val metadata: RecordMetadata = producer.send(message).get()
+        val function = message.commit
+        (metadata, function)
+      }
+
+    }
+    def transform[K2, V2](fn: ProducerMessage[K, V] => ProducerMessage[K2, V2]) = stream map fn
+  }
+
 }
