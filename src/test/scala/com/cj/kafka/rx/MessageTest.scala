0ceaf9501c08e92dc31219240b8d37ac328988be
mary <mflagstad@cj.com>
2015-08-19 16:30:08 -0700

    map keeps key

--- src/test/scala/com/cj/kafka/rx/MessageTest.scala
+++ src/test/scala/com/cj/kafka/rx/MessageTest.scala
@@ -5,16 +5,36 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class MessageTest extends FlatSpec with Matchers {
 
+  val originalMessage = new Message("Key", 1, "topic", 0,0L)
+  def transform(x: Int)  = x + 1
+
+
   "Message" should "be map to a new message with transformed value" in {
     //given
-    val originalMessage = new Message("Key", 1, "topic", 0,0L)
-    def fn(x: Int) = x + 1
 
     //when
-    val newMessage = originalMessage.map(fn)
+    val newMessage = originalMessage.map(transform)
 
     //then
-    newMessage.value should be (fn(originalMessage.value))
+    newMessage.value should be (transform(originalMessage.value))
   }
+
+  "Message" should "keep original key" in {
+
+    // given
+    // (setup)
+
+    // when
+    val newMessage = originalMessage.map(transform)
+
+    // then
+    newMessage.key should be(originalMessage.key)
+
+  }
+
+  "Message" should "keep original topic" in {
+    true should be("not yet implemented")
+  }
+
 
 }
